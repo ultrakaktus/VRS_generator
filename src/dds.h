890370a5bfc33e56474d68f0da7cbb748f09acef
jozkosk1 <jozkosk1@gmail.com>
2016-11-28 19:18:03 +0100

    pridané dds.c  a dds.h

---
+++ src/dds.h
@@ -0,0 +1,19 @@
+/*
+ * dds.h
+ *
+ *  Created on: 28. 11. 2016
+ *      Author: Jozo
+ */
+
+#ifndef DDS_H_
+#define DDS_H_
+#include "stm32l1xx.h"
+
+
+void GPIO_init();
+void SPI_init_by_looser();
+
+
+
+
+#endif /* DDS_H_ */
